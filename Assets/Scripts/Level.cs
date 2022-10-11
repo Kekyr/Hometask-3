b91dfffa058426c3b3fee21d4c542cb916cfd0b3
Mikhail Ostronosov <supermanmgdn@gmail.com>
2022-10-11 16:17:16 +0300

    Added UI for win/lose/endofgame. Made switching of levels.

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -1,94 +1,136 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 namespace Game
 {
-public class Level : MonoBehaviour
-{
-    [Header("Timer")]
-    [SerializeField] private bool _timerIsOn;
-    [SerializeField] private float _timerValue;
-    [SerializeField] private Text _timerView;
-
-    [Header("Objects")]
-    [SerializeField] private Player _player;
-    [SerializeField] private Exit _exitFromLevel;
-    
-    private float _timer = 0;
-    private bool _gameIsEnded = false;
-
-    private void Awake()
+    public class Level : MonoBehaviour
     {
-        _timer = _timerValue;
-    }
+        [Header("Timer")] 
+        [SerializeField] private bool _timerIsOn;
+        [SerializeField] private float _timerValue;
+        [SerializeField] private Text _timerView;
 
-    private void Start()
-    {
-        _exitFromLevel.Close();
-    }
+        [Header("Objects")] 
+        [SerializeField] private Player _player;
+        [SerializeField] private Exit _exitFromLevel;
 
-    private void Update()
-    {
-        if(_gameIsEnded)
-            return;
+        [Header("UI")] 
+        [SerializeField] private GameObject WinWindow;
+        [SerializeField] private GameObject LoseWindow;
+        [SerializeField] private GameObject GameEndWindow;
         
-        TimerTick();
-        LookAtPlayerHealth();
-        LookAtPlayerInventory();
-        TryCompleteLevel();
-    }
+        private int _nextSceneBuildIndex;
+        private int _currentSceneBuildIndex;
+        private float _timer = 0;
+        private bool _gameIsEnded = false;
 
-    private void TimerTick()
-    {
-        if(_timerIsOn == false)
-            return;
-        
-        _timer -= Time.deltaTime;
-        _timerView.text = $"{_timer:F1}";
-        
-        if(_timer <= 0)
+        private void Awake()
+        {
+            _timer = _timerValue;
+            _currentSceneBuildIndex = SceneManager.GetActiveScene().buildIndex;
+        }
+
+        private void Start()
+        {
+            _exitFromLevel.Close();
+        }
+
+        private void Update()
+        {
+            if (_gameIsEnded)
+                return;
+
+            TimerTick();
+            LookAtPlayerHealth();
+            LookAtPlayerInventory();
+            TryCompleteLevel();
+        }
+
+        private void TimerTick()
+        {
+            if (_timerIsOn == false)
+                return;
+
+            _timer -= Time.deltaTime;
+            _timerView.text = $"{_timer:F1}";
+
+            if (_timer <= 0)
+                Lose();
+        }
+
+        private void TryCompleteLevel()
+        {
+            if (_exitFromLevel.IsOpen == false)
+                return;
+
+            var flatExitPosition =
+                new Vector2(_exitFromLevel.transform.position.x, _exitFromLevel.transform.position.z);
+            var flatPlayerPosition = new Vector2(_player.transform.position.x, _player.transform.position.z);
+
+            if (flatExitPosition == flatPlayerPosition)
+                Victory();
+        }
+
+        private void LookAtPlayerHealth()
+        {
+            if (_player.IsAlive)
+                return;
+
             Lose();
-    }
+            Destroy(_player.gameObject);
+        }
 
-    private void TryCompleteLevel()
-    {
-        if(_exitFromLevel.IsOpen == false)
-            return;
+        private void LookAtPlayerInventory()
+        {
+            if (_player.HasKey)
+                _exitFromLevel.Open();
+        }
 
-        var flatExitPosition = new Vector2(_exitFromLevel.transform.position.x, _exitFromLevel.transform.position.z);
-        var flatPlayerPosition = new Vector2(_player.transform.position.x, _player.transform.position.z);
+        public void Victory()
+        {
+            _gameIsEnded = true;
+            _player.Disable();
+            ChooseAndShowWindow();
+        }
+
+        private void ChooseAndShowWindow()
+        {
+            if (_currentSceneBuildIndex == SceneManager.sceneCountInBuildSettings-1)
+            {
+                GameEndWindow.SetActive(true);
+            }
+            else
+            {
+                WinWindow.SetActive(true);
+            }
+        }
+
+        public void Lose()
+        {
+            _gameIsEnded = true;
+            _player.Disable();
+            LoseWindow.SetActive(true);
+        }
         
-        if(flatExitPosition == flatPlayerPosition)
-            Victory();
-    }
+        public void NextLevel()
+        {
+            _nextSceneBuildIndex = _currentSceneBuildIndex + 1;
 
-    private void LookAtPlayerHealth()
-    {
-        if(_player.IsAlive)
-            return;
+            if (_nextSceneBuildIndex >= SceneManager.sceneCountInBuildSettings)
+                return;
+            
+            SceneManager.LoadScene(_nextSceneBuildIndex);
+        }
 
-        Lose();
-        Destroy(_player.gameObject);
+        public void RestartLevel()
+        {
+            SceneManager.LoadScene(_currentSceneBuildIndex);
+        }
+        
+        public void RestartGame()
+        {
+            SceneManager.LoadScene(0);
+        }
     }
-
-    private void LookAtPlayerInventory()
-    {
-        if(_player.HasKey)
-            _exitFromLevel.Open();
-    }
-
-    public void Victory()
-    {
-        _gameIsEnded = true;
-        _player.Disable();
-        Debug.LogError("Victory");
-    }
-
-    public void Lose()
-    {
-        _gameIsEnded = true;
-        _player.Disable();
-        Debug.LogError("Lose");
-    }
-}
 }
